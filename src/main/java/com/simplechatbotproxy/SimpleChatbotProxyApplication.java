0057e658dd55066595c78082016c23ac8b534b68
Song <stalker5217@gmail.com>
2021-02-03 20:46:32 +0900

    first commit

---
+++ src/main/java/com/simplechatbotproxy/SimpleChatbotProxyApplication.java
@@ -0,0 +1,13 @@
+package com.simplechatbotproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChatbotProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleChatbotProxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simplechatbotproxy/SimpleChatbotProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplechatbotproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleChatbotProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
