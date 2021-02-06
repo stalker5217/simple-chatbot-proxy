2d7b25512588c0bfe40ac60ba45cd94e81315123
Song <stalker5217@gmail.com>
2021-02-06 18:16:40 +0900

    QUERY API 구현

--- src/main/java/com/simplechatbotproxy/chat/service/impl/DialogflowCommonService.java
+++ src/main/java/com/simplechatbotproxy/chat/service/impl/DialogflowCommonService.java
@@ -13,9 +13,9 @@ public class DialogflowCommonService {
     public QueryResult detectIntentByEvent(String eventName) throws IOException {
         try (SessionsClient sessionsClient = SessionsClient.create()) {
             UUID uuid = UUID.randomUUID();
-            SessionName session = SessionName.of("song-chat-service", uuid.toString());
+            SessionName sessionName = SessionName.of("song-chat-service", uuid.toString());
 
-            log.info(String.format("Session Path : %s", session.toString()));
+            log.info(String.format("Session Path : %s", sessionName.toString()));
 
             EventInput eventInput = EventInput
                     .newBuilder()
@@ -27,18 +27,43 @@ public class DialogflowCommonService {
                     .newBuilder()
                     .setEvent(eventInput).build();
 
-            DetectIntentResponse response = sessionsClient.detectIntent(session, queryInput);
-            QueryResult queryResult = response.getQueryResult();
-
-            log.info("====================");
-            log.info(String.format("Query Text: %s", queryResult.getQueryText()));
-            log.info(String.format(
-                    "Detected Intent: %s (confidence: %f)",
-                    queryResult.getIntent().getDisplayName(), queryResult.getIntentDetectionConfidence()));
-            log.info(String.format("Fulfillment Text: '%s'", queryResult.getFulfillmentText()));
-            log.info("====================");
-
-            return queryResult;
+            return detectIntent(sessionsClient, sessionName, queryInput);
         }
+    }
+
+    public QueryResult detectIntentByText(String text) throws IOException{
+        try (SessionsClient sessionsClient = SessionsClient.create()) {
+            UUID uuid = UUID.randomUUID();
+            SessionName sessionName = SessionName.of("song-chat-service", uuid.toString());
+
+            log.info(String.format("Session Path : %s", sessionName.toString()));
+
+            TextInput textInput = TextInput
+                    .newBuilder()
+                    .setLanguageCode("ko-KR")
+                    .setText(text)
+                    .build();
+
+            QueryInput queryInput = QueryInput
+                    .newBuilder()
+                    .setText(textInput).build();
+
+            return detectIntent(sessionsClient, sessionName, queryInput);
+        }
+    }
+
+    private QueryResult detectIntent(SessionsClient sessionsClient, SessionName sessionName, QueryInput queryInput){
+        DetectIntentResponse response = sessionsClient.detectIntent(sessionName, queryInput);
+        QueryResult queryResult = response.getQueryResult();
+
+        log.info("====================");
+        log.info(String.format("Query Text: %s", queryResult.getQueryText()));
+        log.info(String.format(
+                "Detected Intent: %s (confidence: %f)",
+                queryResult.getIntent().getDisplayName(), queryResult.getIntentDetectionConfidence()));
+        log.info(String.format("Fulfillment Text: '%s'", queryResult.getFulfillmentText()));
+        log.info("====================");
+
+        return queryResult;
     }
 }
